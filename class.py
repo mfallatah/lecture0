1e9ea5100c567a6a9e1ae75d0477bc318d7e1689
Mohammed Fallatah <mf207@hotmail.com>
2020-07-14 15:55:21 +0300

    n

---
+++ class.py
@@ -0,0 +1,9 @@
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+p = point (3, 5)
+print(p.x)
+print(p.y)
+print(p.x, p.y)
+print("Mohammed")
---
+++ t.py
@@ -0,0 +1,11 @@
+import turtle
+
+def spiral(sides, turn, color, width):
+    t = turtle.Turtle()
+    t.color(color)
+    t.width(width)
+    for n in range(sides):
+        t.forward(n)
+        t.right(turn)
+
+spiral(100, 20, "red", 5)
