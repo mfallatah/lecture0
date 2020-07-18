87390d1835e23b89f279dc331a3b320cef83314e
Mohammed Fallatah <mf207@hotmail.com>
2020-07-18 15:42:46 +0300

    newcs file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace hallo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 30;
+
+
+            Console.WriteLine(a != b);// float to represent the dicemal 
+
+        }
+    }
+}
