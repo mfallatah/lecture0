16b68d6e073e3ff5ccaa060261abb7c77bfa5873
Mohammed Fallatah <mf207@hotmail.com>
2020-07-22 15:47:56 +0300

    databasefile

---
+++ insert.php
@@ -0,0 +1,16 @@
+<?php
+$firstname = $_POST['firstname'];
+$fathername = $_POST['fathername'];
+$thirdname = $_POST['thirdname'];
+$fourthname = $_POST['fourthname'];
+$nid = $_POST['nid'];
+$iddate = $_POST['iddate'];
+$idexp = $_POST['idexp'];
+$born = $_POST['born'];
+$bornl = $_POST['bornl'];
+$mobile = $_POST['mobile'];
+$gender = $_POST['gender'];
+
+if (! empty($firstname) || ! empty($firstname) || ! empty($firstname) || ! empty($firstname) || ! empty($firstname) || ! empty($firstname) || ! empty($firstname) || ! empty($firstname)  )
+
+?>
