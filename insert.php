30990b57f3e823dc5e0bedacd1a9f4047ae75633
Mohammed Fallatah <mf207@hotmail.com>
2020-08-10 15:57:26 +0300

    nnew php file

--- insert.php
+++ insert.php
@@ -1,62 +1,52 @@
 <?php
-$firstname = $_POST['firstname'];
-$fathername = $_POST['fathername'];
-$thirdname = $_POST['thirdname'];
-$fourthname = $_POST['fourthname'];
-$nid = $_POST['nid'];
-$iddate = $_POST['iddate'];
-$idexp = $_POST['idexp'];
-$born = $_POST['born'];
-$bornl = $_POST['bornl'];
-$mobile = $_POST['mobile'];
+$username = $_POST['username'];
+$password = $_POST['password'];
 $gender = $_POST['gender'];
-$gender = $_POST['email'];
+$email = $_POST['email'];
+$phoneCode = $_POST['phoneCode'];
+$phone = $_POST['phone'];
 
-if (! empty($firstname) || ! empty($fathername) || ! empty($thirdname) || ! empty($fourthname) || ! empty($nid) || ! empty($iddate) || ! empty($idexp) || ! empty($born)  || ! empty($bornl) || ! empty($mobile) || i empty($gender) || i empty($email)) {
-      $host = "localhost";
-      $dbUsername = "root";
-      $dbPassword = "";
-      $dbname = "register";
+if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) || !empty($phoneCode) || !empty($phone)) {
+    //تصريح قاعد البيانات
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "youtube";
 
-      // ctreate connection
-      $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
+    //create connetion
+    $conn = new mysqli ($host, $dbUsername, $dbPassword, $dbname);
 
-      if (mysqli_connect_error()) {
-          die('Connect Error('. mysqli_connect_error().')'. mysqli_connect_error());
-      }
-      else {
-          $SELECT = "SELECT email From register Where email = ? Limit 1";
-          $INSERT = "INSERT Into register (firstname, fathername, thiredname, fourthname, nid, iddate, idexp, bornm bornl, mobile, gender, email) " values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+    if (mysqli_connect_error()) {
+        die('Connect Error('.mysqli_connect_errno().')'. mysqli_connect_error());
+    } else {
+        $SELECT = "SELECT email From register Where email = ? Limit 1";
+        $INSERT = "INSERT Into register (username, password, gender, email, phoneCode, phone) values (?, ?, ?, ?, ?, ?)";
 
-          // prepare statement
-          $stmt = $conn->prepare($SELECT);
-          $stmt->bind_param("s", $email);
-          $stmt->execute();
-          $stmt->bind_result($email);
-          $stmt->store_result();
-          $runm = $stmt->num_rows;
+        //Prepare statment
+        $stmt = $conn->prepare($SELECT);
+        $stmt->bind_param("s", $email);   // s because email is a variablr charecer i-intger d-double b-blob
+        $stmt->execute();
+        $stmt->bind_result($email);
+        $stmt->store_result();
+        $rnum = $stmt->num_rows;
 
-          if ($runm==0) {
-              $stmt->close();
-              $stmt->bind_param("ssssiiississ", $firstname, $fathername, $thirdname, $fourthname, $nid, $iddate, $idexp, $born, $bornl, $mobile, $gender, $email);
-              $stmt->execute();
-              echo "New record inserted sucessfully";
+        if ($rnum==0) {
+            $stmt->close();
 
-          }
-          else {
-              echo "Someone alrady register using this email";
-          }
-          $stmt->close();
-          $conn->close();
-      }
-
-
-
-}
-
-else {
+            $stmt = $conn->prepare(INSERT);
+            $stmt->bind_param("ssssii", $username, $password, $gender, $email, $phoneCode, $phoneCode);
+            $stmt->execute();
+            echo "New record inserted sucessfully";
+        } else {
+            echo "Someone already register using this email";
+        }
+        $stmt->close();
+        $conn->close();
+    }
+} else {
     echo "All field are required";
     die();
 }
+
 
 ?>
