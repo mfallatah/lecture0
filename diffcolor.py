0196bdaf11472086008656606f52100255c9ab0c
Mohammed Fallatah <mf207@hotmail.com>
2021-06-17 12:40:58 +0300

    ren

--- diffcolor.py
+++ diffcolor.py
@@ -7,14 +7,15 @@ def tringle_boogie(color,start):
     t.width(5)
     t.right(start)
     for shape in range(6):
-        for side in range(3):
+        for side in range(4):
             t.forward(100)
-            t.right(120)
+            t.right(90)
         t.right(15)
     t.hideturtle()
 
 # call that function three times
 
 tringle_boogie("red",0)
-tringle_boogie("orange", 120)
-tringle_boogie("blue",240)
+tringle_boogie("black",90)
+tringle_boogie("orange",180)
+tringle_boogie("blue",270)
