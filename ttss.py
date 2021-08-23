e6a691675aeb57c6460f99d95091c7c20e093461
Mohammed Fallatah <mf207@hotmail.com>
2021-08-23 08:55:41 +0300

    mn

--- ttss.py
+++ ttss.py
@@ -4,9 +4,14 @@ def ballon(t, color):
     t.speed(5)
     t.color(color)
 
-    for side in range(4):
-         t.forward(100)
+    for side in range(8):
+         t.forward(36)
+         t.right(60)
+         t.forward(12)
          t.right(90)
+         t.forward(12)
+         t.right(60)
+         t.forward(36)
 
 
 
