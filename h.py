105df4420ca763b96c2b519f911ab14d423be10a
mfallatah <mf207@hotmail.com>
2021-09-18 15:31:20 +0300

    gf

---
+++ h.py
@@ -0,0 +1,9 @@
+a= ['Apple','Mango','orange']# list
+b = (91,2,3) # tuple
+fow = {'a', 'b', 'c', 'd'} #set
+abl = {'a' : 'Apple', 'b' : 'Mango', 'c' :'Orang'} #dectonary
+
+print(a)
+print(b)
+print(fow)
+print(abl)
