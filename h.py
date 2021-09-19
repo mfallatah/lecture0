73c2bb74b46597d0ce64303a56dd49409c20573c
mfallatah <mf207@hotmail.com>
2021-09-19 11:29:44 +0300

    jh

--- h.py
+++ h.py
@@ -1,3 +1,4 @@
+'''
 a= ['Apple','Mango','orange']# list
 b = (91,2,3) # tuple
 fow = {'a', 'b', 'c', 'd'} #set
@@ -7,3 +8,42 @@ print(a)
 print(b)
 print(fow)
 print(abl)
+'''
+
+
+'''
+x = "moahmmed"
+print(x.upper())
+
+يحول الحروف كلها الى حروف كبيرة
+'''
+
+
+''''
+x = "moahmmed"
+print(x.lower())
+يحول الحروف الى صغيرة
+'''
+
+"""
+for x in range (30):
+    print(x)
+else:
+    print("Your have tow days of")
+"""
+
+import tkinter
+from tkinter import ttk
+form = tkinter.Tk()
+form.geometry('600x400')
+lbl1 = ttk.Label(form , text = 'MOHAMMED')
+lbl2 = ttk.Label(form , text = 'IBRAHIM')
+lbl3 = ttk.Label(form , text = 'FALLATAH')
+lbl3.config(background='navy',foreground='lightblue',font = ('impact',50),padding =20)
+lbl2.config(background='yellow',foreground='lightblue',font = ('impact',30),padding =(10,20,30,40))
+
+lbl1.pack()
+lbl2.pack()
+lbl3.pack()
+form.mainloop()
+input ('Press Enter')
---
+++ login.py
@@ -0,0 +1,19 @@
+
+username   = input("user name : mo")
+password   = input("password : ")
+repassword = input("Enter password again : ")
+phone      = input("Your phone mumber :")
+birth_day  = input("Enter your birth day : ")
+
+# if & else
+if password == repassword:
+    print("Welcome")
+    print("Your user name is : " + username)
+    print("Your password is created")
+    print("Your phone save")
+    print("Data complated")
+else:
+    print("Your password not matching")
+
+
+# تطبيقها يكون على نفس الصفحة وليس في فورم 
