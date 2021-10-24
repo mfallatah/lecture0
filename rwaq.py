a39f014a29d7492a65120fcadd69932b42de814d
fallatah <mf207@hotmail.com>
2021-10-24 21:39:01 +0300

    k

--- rwaq.py
+++ rwaq.py
@@ -1,13 +1,31 @@
-n = int(input("Enter an Integer: "))
+"""problem 4
+n = int(input())
 
 if n in range(100) and n % 2 != 0: 
-        print ("weird")
+        print ("Weird")
 if n in range(2-5) and n % 2 ==0:
-		print("Not weird")
+        print("Not Weird")
 if n in range(6-20) and n % 2 == 0 :
-	print("weird")
+        print("Weird")
 if n % 2 == 0 and n > 20 :
-	print("Not weird")
+        print ("Not Weird")
 else:
-	pass
+    pass
 
+
+problem 4
+n = int(raw_input())
+
+if n%2==0 and (n in range(2,6) or n>20 ):
+    print("Not Weird")
+else:
+    print("Weird")
+    
+
+problem 5
+"""
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(0,n): 
+        i= i**2 
+        print(i)
