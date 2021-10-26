ac850e568d5e1be30a2c7b2533c32b7d6f6d84e9
mfallatah <mf207@hotmail.com>
2021-10-26 14:13:43 +0300

    m

--- data_entry.py
+++ data_entry.py
@@ -5,19 +5,30 @@ import pandas as pd
 
 sg.theme('Black')
 
+EXCEL_FILE = 'data_entry.xlsx'
+df = pd.read_excel(EXCEL_FILE)
+
 layout = [
-[sg.Text('Plaese Fill Out The Following Filed')],
+[sg.Text('Plaese Fill Out The Following Filed')],# الكلمات الرئيسية لازم تكونتبدأ بحروف كبيرة Text,Combo,..
 [sg.Text('Name',size=(15,1)), sg.InputText(key='Name')],
-[sg.Submit(), sg.Exit()]
+[sg.Text('Favorte Color', size=(15,1)), sg.Combo(['Green','Red','Blue'], key='Favotite Color')],
+[sg.Text('I speak', size=(15,1)),
+                          sg.Checkbox('Arabic', key='Arabic'),
+                          sg.Checkbox('English', key='English'),
+                          sg.Checkbox('Italy', key='Italy')],
+[sg.Text('No. of children', size=(15,1)), sg.Spin([i for i in range(0,16)],
+                                                                   initial_value=0, key='Children')],
+[sg.Submit(), sg.Exit()] # زر الراسال وزر الاغلاق
 ]
 window = sg.Window('Simple data entry form',layout)
 
-
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
+    if event == sg.WIN_CLOSED or event == 'Exit': # زر الاغلاق او علامة الاكس
+        break # اذا حصل احد الشرطين اللي فوق انهي البرنامج
 
-    if event == 'Submit':
-        print(event, values)        
+    if event == 'Submit':  # اذا كان الحدث الضغط على زر  submit يطبع قيمة values
+        df = df.append(values, ignore_index=True)
+        df.to_excel(EXCEL_FILE, index=False)
+        sg.popup('Data saved!')
 window.close()
--- ss.py
+++ ss.py
@@ -1,6 +1,11 @@
-import PySimpleGUI as sg
 
-sg.theme('darktea19')
+
+"""
+import PySimpleGUI as sg
+import pandas as pd
+
+
+sg.theme('Black')
 
 layout = [
 [sg.Text('Plaese Fill Out The Following Filed')],
@@ -10,7 +15,47 @@ layout = [
 window = sg.Window('Simple data entry form',layout)
 
 
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit':
+        break
+
+    if event == 'Submit':
+        print(event, values)
+window.close()
 
 
+"""
+import PySimpleGUI as sg
+import pandas as pd
+
+
+sg.theme('Black')
+
+EXCEL_FILE = 'data_entry.xlsx'
+df = pd.read_excel(EXCEL_FILE)
+
+layout = [
+[sg.Text('Plaese Fill Out The Following Filed')],# الكلمات الرئيسية لازم تكونتبدأ بحروف كبيرة Text,Combo,..
+[sg.Text('Name',size=(15,1)), sg.InputText(key='Name')],
+[sg.Text('Favorte Color', size=(15,1)), sg.Combo(['Green','Red','Blue'], key='Favotite Color')],
+[sg.Text('I speak', size=(15,1)),
+                          sg.Checkbox('Arabic', key='Arabic'),
+                          sg.Checkbox('English', key='English'),
+                          sg.Checkbox('Italy', key='Italy')],
+[sg.Text('No. of children', size=(15,1)), sg.Spin([i for i in range(0,16)],
+                                                                   initial_value=0, key='Children')],
+[sg.Submit(), sg.Exit()] # زر الراسال وزر الاغلاق
+]
+window = sg.Window('Simple data entry form',layout)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Exit': # زر الاغلاق او علامة الاكس
+        break # اذا حصل احد الشرطين اللي فوق انهي البرنامج
+
+    if event == 'Submit':  # اذا كان الحدث الضغط على زر  submit يطبع قيمة values
+        print(event, values)
+window.close()
 
 # https://www.youtube.com/watch?v=svcv8uub0D0
