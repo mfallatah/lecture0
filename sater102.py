ff7e4d6cb3d5fff148c0c2bb55c3e6ddab29c3f1
mfallatah <50276899+mfallatah@users.noreply.github.com>
2022-02-08 08:47:53 +0300

    Create sater102.py

---
+++ sater102.py
@@ -0,0 +1,22 @@
+number = -999
+print(abs(number)) # To print abslute value 
+
+number1 =  53.366
+print(round(number1, 2)) # number 2 means take seconed digit after coma
+
+num = 3
+print(pow(num, 2))# To power of number
+
+print( pow(3,3)) # another way
+
+num4= (100,200,333,444) # To print maximmum value
+print(max(num4))
+
+num5= (100,200,333,444) # To print minimmum value
+print(min(num5))
+
+print(min(100,200,333,444))# another way
+
+import math
+num6=144
+print(math.sqrt(num6)) # to print squre root we must invite math librarry
